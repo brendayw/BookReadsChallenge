d66dc5ea5d50e8e7aec5697d2f59ed10eaf49a8c
brendayw <brendayw97@gmail.com>
2025-08-02 21:29:16 -0300

    primer commit del challenge

---
+++ src/main/java/com/ar/BookReads/BookReadsApplication.java
@@ -0,0 +1,13 @@
+package com.ar.BookReads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookReadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookReadsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ar/BookReads/BookReadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ar.BookReads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
